8d41b3c598035007344f4c5888edafa889e1b198
aafomin <alexcode@mail.ru>
2019-11-26 13:04:00 +0500

    add style in ThemeItem

--- WebReactSite/ClientApp/src/components/Forum/ThemeItem.jsx
+++ WebReactSite/ClientApp/src/components/Forum/ThemeItem.jsx
@@ -4,6 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,7 +15,16 @@ const useStyles = makeStyles(theme => ({
   paper: {
     maxWidth: "80%",
     margin: `${theme.spacing(1)}px auto`,
-    padding: theme.spacing(2)
+    padding: theme.spacing(2),
+    background: "#f9f9f9"
+  },
+  icon: {
+    fontSize: "2.5rem",
+    color: "primary"
+  },
+
+  counter: {
+    marginTop: "25%"
   }
 }));
 
@@ -22,18 +32,22 @@ const ThemeItem = props => {
   const classes = useStyles();
   const { header, description, icon } = props.theme;
 
-  const Icon = React.createElement(Font[icon], {
-    style: { fontSize: "2.5rem", color: "primary" }
-  });
+  const Icon = React.createElement(Font[icon], { className: classes.icon });
   return (
     <div className={classes.root}>
+      <CssBaseline />
       <Paper className={classes.paper}>
         <Grid container wrap="nowrap" spacing={2}>
           <Grid item>{Icon}</Grid>
           <Grid item xs>
-            <Typography>{header}</Typography>
-            <Typography>{description}</Typography>
+            <Typography variant="h5">{header}</Typography>
+            <Typography variant="body1">{description}</Typography>
           </Grid>
+          {/* <Grid item>
+            <Typography variant="body2" className={classes.counter}>
+              Количество постов: 0
+            </Typography>
+          </Grid> */}
         </Grid>
       </Paper>
     </div>
