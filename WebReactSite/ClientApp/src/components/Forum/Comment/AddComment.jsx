8e23ca00df8d1aa7617fb7cbd21306e41f446b79
aafomin <alexcode@mail.ru>
2019-12-02 11:37:56 +0500

    addComment in comment component

--- WebReactSite/ClientApp/src/components/Forum/Comment/AddComment.jsx
+++ WebReactSite/ClientApp/src/components/Forum/Comment/AddComment.jsx
@@ -1,41 +1,64 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-import Tooltip from '@material-ui/core/Tooltip';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-
-// const [open, setOpen] = useState(false);
-// const openText = () => setOpen(!open);
-// <AddPost openText={openText} open={open}/>
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Tooltip from "@material-ui/core/Tooltip";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import SaveIcon from "@material-ui/icons/Save";
 
 const useStyles = makeStyles(theme => ({
   fab: {
-    margin: theme.spacing(2),
-  },
-  absolute: {
-    position: 'absolute',
-    bottom: theme.spacing(2),
-    right: theme.spacing(3),
+    margin: theme.spacing(2)
   },
   textArea: {
-    width: "80%",
+    width: "90%",
     marginTop: theme.spacing(2)
   }
 }));
 
-const AddComment = (props) => {
+const AddComment = props => {
   const { openText, open } = props;
   const classes = useStyles();
   return (
-    <div>
-      <Tooltip title="Add Commment" aria-label="add" onClick={() => openText()}>
-        <Fab color="primary" className={classes.fab}>
-          <AddIcon />
-        </Fab>
-      </Tooltip>
-      {open && <TextareaAutosize aria-label="minimum height" rows={3} placeholder="Enter Your Comment" className={classes.textArea} />}
-    </div>
-  )
-}
+    <Grid container spacing={1} wrap="nowrap">
+      <Grid item xs={2} sm={1}>
+        <Tooltip
+          title="Add Commment"
+          aria-label="add"
+          onClick={() => openText()}
+        >
+          <Fab color="primary" className={classes.fab}>
+            <AddIcon />
+          </Fab>
+        </Tooltip>
+      </Grid>
+      {open && (
+        <Grid item xs={11} sm={8}>
+          <TextareaAutosize
+            aria-label="minimum height"
+            rows={5}
+            placeholder="Enter Your Comment"
+            className={classes.textArea}
+            value={props.input}
+            onChange={e => props.setInput(e.target.value)}
+          />
+          <div>
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              className={classes.button}
+              // onClick={() => }
+              startIcon={<SaveIcon />}
+            >
+              Save
+            </Button>
+          </div>
+        </Grid>
+      )}
+    </Grid>
+  );
+};
 export default AddComment;
--- WebReactSite/ClientApp/src/components/Forum/Comment/Comment.jsx
+++ WebReactSite/ClientApp/src/components/Forum/Comment/Comment.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AddComment from "./AddComment";
+import Divider from "@material-ui/core/Divider";
 
 const Comment = props => {
-  const { forumId } = props.match.params;
+  // const { forumId } = props.match.params;
   const search = new URLSearchParams(props.location.search);
   const postId = search.get("postId");
-  console.log(postId);
-  return <div>{postId}</div>;
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+  const openText = () => setOpen(!open);
+
+  return (
+    <div>
+      <div></div>
+      <Divider variant="middle" />
+      <AddComment
+        openText={openText}
+        open={open}
+        input={input}
+        setInput={setInput}
+      />
+    </div>
+  );
 };
 
 export default Comment;
