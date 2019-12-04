3b1d394549c5502570416ce0e2d5bc6cff30eed7
aafomin <alexcode@mail.ru>
2019-12-04 17:14:54 +0500

    add CommentItem and change Comments Model

---
+++ WebReactSite/ClientApp/src/components/Forum/Comment/CommentItem.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: "hidden",
+    padding: theme.spacing(0, 3),
+    border: "solid 1px rgba(72, 76, 84, 0.68)",
+    borderRadius: ".5rem",
+    marginBottom: theme.spacing(2),
+    background: "whitesmoke"
+  }
+}));
+
+const CommentItem = props => {
+  const classes = useStyles();
+  const { comment } = props;
+  return (
+    <Grid container wrap="nowrap" spacing={2} className={classes.root}>
+      <Grid item>
+        <Avatar alt={comment.user} src={comment.userAvatar} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CommentItem;
--- WebReactSite/Models/Comment.cs
+++ WebReactSite/Models/Comment.cs
@@ -12,5 +12,6 @@ namespace WebReactSite.Models
         public string Body { get; set; }
         public string Author { get; set; }
         public DateTime CreateDate { get; set; }
+        public string AuthorAvatar { get; set; }
     }
 }
